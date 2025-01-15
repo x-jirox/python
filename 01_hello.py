a20e56f42b2d5d9cc0313f07efaab632e805885c
x-jirox <correalenin045gmail.com>
2025-01-14 20:44:43 -0500

    py ejercicios

---
+++ 01_hello.py
@@ -0,0 +1,19 @@
+"""
+comentario 
+en varias 
+lineas
+"""
+
+#comentario en una sola linea
+
+#Prints
+print("hello python")
+
+print('Hello python')
+
+#Consulta el typo de dato usando type
+print(type("Hello python")) #tipo string
+print(type(5)) #tipo int
+print(type(1.5)) #tipo float
+print(type(True)) #tipo boolean
+print(type(1 + 5j)) #tipo complex
---
+++ 02_variables.py
@@ -0,0 +1,44 @@
+#variables
+
+my_string_variable="Mi vriable string"
+print(my_string_variable)
+
+my_int_variable=5
+print(my_int_variable)
+
+my_float_variable=5.5
+print(my_float_variable)
+
+my_boolean_variable=True
+print(my_boolean_variable)
+
+
+#Concatenacion de variables en un print
+print(my_string_variable, " , " ,my_int_variable," , " , my_float_variable, " , " , my_boolean_variable)
+
+
+#variables utilizando str
+my_int_variable_str=str(my_int_variable)
+print(my_int_variable)
+print(type(my_int_variable))
+
+
+#funciones del sistema, cuenta la cadena
+print(len(my_string_variable))
+
+
+#concatenacion de variables con print
+print("este es el integer de mi variable: ", my_int_variable)
+
+
+#variables en una sola linea , no se debe abusar de esta centencia
+first_name, second_name, alias, age= "Lenin","Correa","jiro",21
+print ("Me llamo: ",first_name," ", second_name," , ","Mi alias es : ", alias," , ","Mi edad es : ", age)
+
+
+#Uso de imputs
+first_name_imput= input("what is your name: ")
+age_input= input("What is your age: ")
+
+print("Hello: ",first_name_imput)
+print("Your age is: ",age)
