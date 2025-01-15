ca78c85c4374c9083e8523db362ff0e101f720c9
x-jirox <correalenin045gmail.com>
2025-01-14 21:48:04 -0500

    py ejercicios

--- 02_variables.py
+++ 02_variables.py
@@ -11,29 +11,35 @@ print(my_float_variable)
 
 my_boolean_variable=True
 print(my_boolean_variable)
+print("-----------------------------")
 
 
 #Concatenacion de variables en un print
 print(my_string_variable, " , " ,my_int_variable," , " , my_float_variable, " , " , my_boolean_variable)
+print("-----------------------------")
 
 
 #variables utilizando str
 my_int_variable_str=str(my_int_variable)
 print(my_int_variable)
 print(type(my_int_variable))
+print("-----------------------------")
 
 
 #funciones del sistema, cuenta la cadena
 print(len(my_string_variable))
+print("-----------------------------")
 
 
 #concatenacion de variables con print
 print("este es el integer de mi variable: ", my_int_variable)
+print("-----------------------------")
 
 
 #variables en una sola linea , no se debe abusar de esta centencia
 first_name, second_name, alias, age= "Lenin","Correa","jiro",21
 print ("Me llamo: ",first_name," ", second_name," , ","Mi alias es : ", alias," , ","Mi edad es : ", age)
+print("-----------------------------")
 
 
 #Uso de imputs
---
+++ 03_operators.py
@@ -0,0 +1,60 @@
+#OPERADORES ARITMETICOS
+
+#operaciones
+print(5 + 3)
+print(5 - 3)
+print(5 * 3)
+print(5 / 2)
+
+print("-----------------------------")
+
+#modulos
+print(10 % 3)
+print(17 // 3)
+print(5 ** 2)
+
+print("-----------------------------")
+
+
+
+#union de cadenas
+print("Hola " + "Python")
+
+#forzar que se use como string con str
+print("Hola " + str(5))
+
+#multiplica la cadena
+print("Hola " * 5)
+print("Hola " * (2**2))
+
+#uso de variable con operaciones
+my_float = 2.5 * 2
+print("Hola "* int(my_float))
+
+print("-----------------------------")
+
+
+
+
+
+#OPERADORES COMPARATIVOS
+
+#comparacion de integers
+print(5 < 3)
+print(5 > 3)
+print(5 <= 3)
+print(5 >= 3)
+print(5 == 3)
+print(5 != 3)
+
+print("-----------------------------")
+
+
+
+
+
+#OPERADORES LOGICOS
+
+print(3 < 5 and "Hola" < "Python")
+print(3 > 5 or "Hola" > "Python")
+print(not(3 > 4) )
